af424d50616e50f3cbf2db8f0f04599482ce8609
Jamie King <jamieking@flashtract.com>
2023-09-30 10:03:04 -0400

    making auth callback

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,46 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import Link  from 'next/link';
 import { buttonVariants } from './ui/button';
+import {LoginLink, RegisterLink} from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
 const Navbar = () => {
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-b-gray-2 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
-                                                <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+                <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
                     <Link href='/' className='flex z-40 font-semibold'>
                         <span>quill.</span>
                     </Link>
-                </div>
-            </MaxWidthWrapper>
-            
-            <div className='hidden items-center space-x-4 sm:flex'>
+                    <div className='hidden items-center space-x-4 sm:flex'>
                 <>
                     <Link
                         className={buttonVariants({
-                        variant: 'ghost',
-                        size: 'sm',
-                })} 
-                href='/pricing'>Pricing</Link> 
+                            variant: 'ghost',
+                            size: 'sm',
+                        })}
+                        href='/pricing'>Pricing</Link>
+                    <LoginLink
+                        className={buttonVariants({
+                            variant: 'ghost',
+                            size: 'sm',
+                        })}
+                    >
+                        Sign in
+                    </LoginLink>
+                    <RegisterLink
+                        className={buttonVariants({
+                            size: 'sm',
+                        })}
+                    >
+                        Get started <ArrowRight className='ml-1.5 h-5 w-5'/>
+                    </RegisterLink>
+
                 </>
             </div>
+                </div>
+            </MaxWidthWrapper>
         </nav>
     )
-}
+};
 
 export default Navbar
