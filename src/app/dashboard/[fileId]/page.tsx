b6eeac968964f6fc45de3509d9b101f65da861bc
Jamie King <jamieking@flashtract.com>
2023-09-30 14:39:02 -0400

    can show pdfs

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,5 +1,6 @@
 // import ChatWrapper from '@/components/chat/ChatWrapper'
 // import PdfRenderer from '@/components/PdfRenderer'
+import PdfRenderer from '@/components/PdfRenderer'
 import { db } from '@/db'
 // import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
@@ -7,22 +8,22 @@ import { notFound, redirect } from 'next/navigation'
 
 interface PageProps {
   params: {
-    fileid: string
+    fileId: string
   }
 }
 
 const Page = async ({ params }: PageProps) => {
-  const { fileid } = params
+  const { fileId } = params
 
   const { getUser } = getKindeServerSession()
   const user = getUser()
 
   if (!user || !user.id)
-    redirect(`/auth-callback?origin=dashboard/${fileid}`)
+    redirect(`/auth-callback?origin=dashboard/${fileId}`)
 
   const file = await db.file.findFirst({
     where: {
-      id: fileid,
+      id: fileId,
       userId: user.id,
     },
   })
@@ -30,15 +31,14 @@ const Page = async ({ params }: PageProps) => {
   if (!file) notFound()
 
 //   const plan = await getUserSubscriptionPlan()
-
+  console.log({file})
   return (
     <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
       <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
         {/* Left sidebar & main wrapper */}
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
-            {/* Main area */}
-            {/* <PdfRenderer url={file.url} /> */}
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
